9fafd3c12948ec628aeda21f0e214215075f65d2
JackSaf <jacksafblaze@gmail.com>
2022-10-11 18:56:28 +0300

    added viewModel and binding

--- app/src/main/java/com/jacksafblaze/storeowner/presentation/LoginFragment.kt
+++ app/src/main/java/com/jacksafblaze/storeowner/presentation/LoginFragment.kt
@@ -1,10 +1,31 @@
 package com.jacksafblaze.storeowner.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.jacksafblaze.storeowner.R
+import com.jacksafblaze.storeowner.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment: Fragment(R.layout.fragment_login) {
+    val viewModel: LoginViewModel by viewModels()
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
