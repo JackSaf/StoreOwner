af79bd1cd00ffc23c6b5c645e24a634e098468e2
JackSaf <jacksafblaze@gmail.com>
2022-10-11 15:09:39 +0300

    added uiState as a view model field

--- app/src/main/java/com/jacksafblaze/storeowner/presentation/LoginViewModel.kt
+++ app/src/main/java/com/jacksafblaze/storeowner/presentation/LoginViewModel.kt
@@ -1,6 +1,10 @@
 package com.jacksafblaze.storeowner.presentation
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class LoginViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState = _uiState.asStateFlow()
 }
