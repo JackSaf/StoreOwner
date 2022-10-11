c1f239e296b4de3b1d607fa1725a94e2809dbb13
JackSaf <jacksafblaze@gmail.com>
2022-10-11 18:57:20 +0300

    refactored login and register functions

--- app/src/main/java/com/jacksafblaze/storeowner/data/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/jacksafblaze/storeowner/data/repository/LoginRepositoryImpl.kt
@@ -1,24 +1,31 @@
 package com.jacksafblaze.storeowner.data.repository
 
+import com.google.android.gms.common.api.Response
 import com.google.firebase.auth.FirebaseAuth
 import com.jacksafblaze.storeowner.domain.repository.LoginRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class LoginRepositoryImpl(val firebaseAuth: FirebaseAuth): LoginRepository {
-    override suspend fun isUserLoggedIn(): Boolean {
-        return firebaseAuth.currentUser != null
-    }
+class LoginRepositoryImpl(private val firebaseAuth: FirebaseAuth): LoginRepository {
+    override fun isUserLoggedIn() = firebaseAuth.currentUser != null
 
     override suspend fun login(email: String, password: String): Boolean {
         return withContext(Dispatchers.IO){
-            firebaseAuth.signInWithEmailAndPassword(email, password).isSuccessful
+            val result = firebaseAuth.signInWithEmailAndPassword(email, password)
+            result.exception?.let {
+                throw it.cause!!
+            }
+            result.isSuccessful
         }
     }
 
     override suspend fun register(email: String, password: String): Boolean {
         return withContext(Dispatchers.IO) {
-            firebaseAuth.createUserWithEmailAndPassword(email, password).isSuccessful
+            val result = firebaseAuth.createUserWithEmailAndPassword(email, password)
+            result.exception?.let {
+                throw it.cause!!
+            }
+            result.isSuccessful
         }
     }
 
