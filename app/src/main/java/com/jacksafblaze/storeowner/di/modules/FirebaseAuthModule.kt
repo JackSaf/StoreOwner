6b40ac78fa10641321325d32c0a123539b9a164a
JackSaf <jacksafblaze@gmail.com>
2022-10-11 19:27:17 +0300

    created di modules

---
+++ app/src/main/java/com/jacksafblaze/storeowner/di/modules/FirebaseAuthModule.kt
@@ -0,0 +1,20 @@
+package com.jacksafblaze.storeowner.di.modules
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class FirebaseAuthModule {
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth{
+        return Firebase.auth
+    }
+}
