eb1618e88f068d76ec1893c32999ea84a2db0e18
JackSaf <jacksafblaze@gmail.com>
2022-10-14 12:23:41 +0300

    used await function in firebase

--- app/src/main/java/com/jacksafblaze/storeowner/data/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/jacksafblaze/storeowner/data/repository/LoginRepositoryImpl.kt
@@ -1,9 +1,9 @@
 package com.jacksafblaze.storeowner.data.repository
 
-import com.google.android.gms.common.api.Response
 import com.google.firebase.auth.FirebaseAuth
 import com.jacksafblaze.storeowner.domain.repository.LoginRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class LoginRepositoryImpl(private val firebaseAuth: FirebaseAuth): LoginRepository {
@@ -11,21 +11,15 @@ class LoginRepositoryImpl(private val firebaseAuth: FirebaseAuth): LoginReposito
 
     override suspend fun login(email: String, password: String): Boolean {
         return withContext(Dispatchers.IO){
-            val result = firebaseAuth.signInWithEmailAndPassword(email, password)
-            result.exception?.let {
-                throw it.cause!!
-            }
-            result.isSuccessful
+            firebaseAuth.signInWithEmailAndPassword(email, password).await()
+            true
         }
     }
 
     override suspend fun register(email: String, password: String): Boolean {
         return withContext(Dispatchers.IO) {
-            val result = firebaseAuth.createUserWithEmailAndPassword(email, password)
-            result.exception?.let {
-                throw it.cause!!
-            }
-            result.isSuccessful
+            firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+            true
         }
     }
 
