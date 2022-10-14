2ed695b3717f4c87cce7abe5106413a437b83eed
JackSaf <jacksafblaze@gmail.com>
2022-10-14 13:24:40 +0300

    changed LoginRepositoryImpl to LoginRepository

--- app/src/main/java/com/jacksafblaze/storeowner/domain/usecaseimpl/SendVerificationEmailUseCaseImpl.kt
+++ app/src/main/java/com/jacksafblaze/storeowner/domain/usecaseimpl/SendVerificationEmailUseCaseImpl.kt
@@ -1,9 +1,9 @@
 package com.jacksafblaze.storeowner.domain.usecaseimpl
 
-import com.jacksafblaze.storeowner.data.repository.LoginRepositoryImpl
+import com.jacksafblaze.storeowner.domain.repository.LoginRepository
 import com.jacksafblaze.storeowner.domain.usecase.SendVerificationEmailUseCase
 
-class SendVerificationEmailUseCaseImpl(private val repositoryImpl: LoginRepositoryImpl) : SendVerificationEmailUseCase {
+class SendVerificationEmailUseCaseImpl(private val repositoryImpl: LoginRepository) : SendVerificationEmailUseCase {
     override suspend fun execute(): Boolean {
         return repositoryImpl.sendVerificationEmail()
     }
