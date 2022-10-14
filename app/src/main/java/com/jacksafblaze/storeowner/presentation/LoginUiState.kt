0d1fc71d32a78c6230e7c1476298a0677c22cfa8
JackSaf <jacksafblaze@gmail.com>
2022-10-14 12:23:55 +0300

    added new fields to state

--- app/src/main/java/com/jacksafblaze/storeowner/presentation/LoginUiState.kt
+++ app/src/main/java/com/jacksafblaze/storeowner/presentation/LoginUiState.kt
@@ -4,6 +4,10 @@ data class LoginUiState(
     val email: String? = null,
     val password: String? = null,
     val isLoading: Boolean = false,
+    val isEmailOk: Boolean = false,
+    val isPasswordOk: Boolean = false,
     val emailAlert: String? = null,
     val passwordAlert: String? = null,
-    val message: String? = null)
+    val message: String? = null){
+    val buttonsEnabled get() = isEmailOk && isPasswordOk
+}
