995194beb868398cd0667ecd6e6fb3d9478d59ba
JackSaf <jacksafblaze@gmail.com>
2022-10-19 02:46:25 +0300

    made checkIfUserVerifiedUseCaseImpl implement it's interface

--- app/src/main/java/com/jacksafblaze/storeowner/domain/usecaseimpl/CheckIfUserVerifiedUseCaseImpl.kt
+++ app/src/main/java/com/jacksafblaze/storeowner/domain/usecaseimpl/CheckIfUserVerifiedUseCaseImpl.kt
@@ -1,10 +1,11 @@
 package com.jacksafblaze.storeowner.domain.usecaseimpl
 
 import com.jacksafblaze.storeowner.domain.repository.LoginRepository
+import com.jacksafblaze.storeowner.domain.usecase.CheckIfUserVerifiedUseCase
 import kotlinx.coroutines.flow.Flow
 
-class CheckIfUserVerifiedUseCaseImpl(private val repository: LoginRepository) {
-    fun execute(): Flow<Boolean> {
+class CheckIfUserVerifiedUseCaseImpl(private val repository: LoginRepository): CheckIfUserVerifiedUseCase {
+    override fun execute(): Flow<Boolean> {
         return repository.isUserVerified()
     }
 }
