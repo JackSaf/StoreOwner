ff0c2cac3d4ce9ff49dcecf645a03cae711512c9
JackSaf <jacksafblaze@gmail.com>
2022-10-18 21:57:41 +0300

    added and implemented isUserVerified function

--- app/src/main/java/com/jacksafblaze/storeowner/data/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/jacksafblaze/storeowner/data/repository/LoginRepositoryImpl.kt
@@ -1,13 +1,28 @@
 package com.jacksafblaze.storeowner.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener
 import com.jacksafblaze.storeowner.domain.repository.LoginRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class LoginRepositoryImpl(private val firebaseAuth: FirebaseAuth): LoginRepository {
-    override fun isUserLoggedIn() = firebaseAuth.currentUser != null
+
+    override fun isUserLoggedIn(): Flow<Boolean> = callbackFlow{
+        val listener = AuthStateListener { trySend(firebaseAuth.currentUser != null) }
+        firebaseAuth.addAuthStateListener(listener)
+        awaitClose { firebaseAuth.removeAuthStateListener(listener) }
+    }
+
+    override fun isUserVerified(): Flow<Boolean> = callbackFlow {
+        val listener = AuthStateListener { trySend(firebaseAuth.currentUser?.isEmailVerified ?: false) }
+        firebaseAuth.addAuthStateListener(listener)
+        awaitClose { firebaseAuth.removeAuthStateListener(listener) }
+    }
 
     override suspend fun login(email: String, password: String): Boolean {
         return withContext(Dispatchers.IO){
@@ -35,7 +50,7 @@ class LoginRepositoryImpl(private val firebaseAuth: FirebaseAuth): LoginReposito
         }
     }
 
-    override suspend fun logout() {
+    override fun logout() {
         firebaseAuth.signOut()
     }
 }
