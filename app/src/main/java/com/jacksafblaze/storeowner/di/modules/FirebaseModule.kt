027e89ad3fd949dca010dd7fb571796e1122dfff
JackSaf <jacksafblaze@gmail.com>
2022-11-07 00:11:03 +0300

    renamed module + added functions

--- app/src/main/java/com/jacksafblaze/storeowner/di/modules/FirebaseModule.kt
+++ app/src/main/java/com/jacksafblaze/storeowner/di/modules/FirebaseModule.kt
@@ -2,7 +2,11 @@ package com.jacksafblaze.storeowner.di.modules
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,10 +15,22 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class FirebaseAuthModule {
+class FirebaseModule {
     @Provides
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth{
         return Firebase.auth
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseDatabase(): FirebaseDatabase{
+        return Firebase.database
+    }
+
+    @Provides
+    @Singleton
+    fun provideCloudStorage(): FirebaseStorage{
+        return Firebase.storage
     }
 }
