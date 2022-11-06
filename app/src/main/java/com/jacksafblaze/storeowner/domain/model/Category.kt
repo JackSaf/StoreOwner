a753851a01071be27526a55fbcc905eeb1521943
JackSaf <jacksafblaze@gmail.com>
2022-11-06 20:28:07 +0300

    created product category class

---
+++ app/src/main/java/com/jacksafblaze/storeowner/domain/model/Category.kt
@@ -0,0 +1,5 @@
+package com.jacksafblaze.storeowner.domain.model
+
+data class Category(val id: String = "",
+                    val name: String = "",
+                    val imageUri: String = "")
