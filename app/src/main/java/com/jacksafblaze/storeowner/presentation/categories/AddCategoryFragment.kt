d3d161eb2afcbd24fdf229f94dd7216e0d38429b
JackSaf <jacksafblaze@gmail.com>
2022-11-06 18:46:35 +0300

    created add category fragment

---
+++ app/src/main/java/com/jacksafblaze/storeowner/presentation/categories/AddCategoryFragment.kt
@@ -0,0 +1,96 @@
+package com.jacksafblaze.storeowner.presentation.categories
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import com.jacksafblaze.storeowner.databinding.FragmentAddCategoryBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+
+class AddCategoryFragment : Fragment() {
+    val viewModel: AddCategoryViewModel by viewModels()
+    private var _binding: FragmentAddCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    private val getContent : ActivityResultLauncher<String> = registerForActivityResult(
+        ActivityResultContracts.GetContent()){ imageUri ->
+        viewModel.setImage(imageUri = imageUri)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddCategoryBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindState()
+        binding.name.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                viewModel.setName(p0.toString())
+            }
+
+        })
+        binding.addImage.setOnClickListener{
+            getContent.launch("image/*")
+        }
+        binding.add.setOnClickListener {
+            viewModel.addCategory()
+        }
+        binding.cancel.setOnClickListener {
+
+        }
+    }
+
+    private fun bindState(){
+        viewLifecycleOwner.lifecycleScope.launch{
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.uiState.collectLatest { state ->
+                    if(state.imageUri != null){
+                        Glide.with(requireView()).load(state.imageUri).into(binding.image)
+                    }
+                    if(state.isCategoryAdded){
+
+                    }
+                    if(!state.message.isNullOrBlank()){
+                        Snackbar.make(requireView(), state.message, Snackbar.LENGTH_LONG).show()
+                        viewModel.userMessageShown()
+                    }
+                    if(state.isNameOk){
+                        binding.nameAlert.visibility = View.GONE
+                    }
+                    else{
+                        binding.nameAlert.visibility = View.VISIBLE
+                        binding.nameAlert.text = state.nameAlert
+                    }
+                    binding.add.isEnabled = state.isAddButtonEnabled
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+}
